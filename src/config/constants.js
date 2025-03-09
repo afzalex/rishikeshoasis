a188bcf2c1dde9bc8ae8e9377fd335e71a4756c9
Mohammad Afzal <afzalex.store@gmail.com>
2025-03-10 04:12:12 +0530

    Updated base url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,12 +1,12 @@
 export const getBaseUrl = () => {
   return process.env.NODE_ENV === 'production' 
-    ? '/rishikeshoasis'
+    ? ''
     : '';
 };
 
 // For assets like images
 export const getAssetsUrl = () => {
   return process.env.NODE_ENV === 'production' 
-    ? '/rishikeshoasis'
+    ? ''
     : '';
 }; 
